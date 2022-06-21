9d2cc7cf9a81ecf6b0bb01c47e73bd81bc0aab09
Richard P Machado <richardmachado2211@gmail.com>
2022-06-21 19:26:49 -0300

    exercício número 02

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercícios/exercicio-02.js
@@ -0,0 +1,10 @@
+const a = 100
+const b = 99
+
+if (a > b){
+    console.log(`O número A =${a} é maior que o número B =${b}`);
+} else if (a < b) {
+    console.log(`O número B =${b} é maior que o número A =${a}`);
+} else {
+    console.log("Os valores A e B são iguais");
+}
