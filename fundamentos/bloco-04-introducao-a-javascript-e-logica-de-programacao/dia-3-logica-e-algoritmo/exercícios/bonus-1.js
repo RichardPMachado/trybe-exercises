043de44e5d7eb28466a5416463d4255b62e61072
Richard P Machado <richardmachado2211@gmail.com>
2022-06-23 19:33:17 -0300

    imprimir na tela um quadrado feito de asteriscos de tamanho n

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-e-algoritmo/exercícios/bonus-1.js
@@ -0,0 +1,10 @@
+let n = 5
+
+
+for (let linha = 0; linha < n; linha+=1) {
+    let square = ""
+    for (let linha2 = 0; linha2 < n; linha2+=1){
+            square += "*" 
+    }
+    console.log(square);
+}
