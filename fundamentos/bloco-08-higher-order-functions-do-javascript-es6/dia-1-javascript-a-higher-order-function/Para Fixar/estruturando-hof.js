f5ed7614b628bc8b5db5d084ce80c5c4ebec4fb7
Richard P Machado <richardmachado2211@gmail.com>
2022-07-21 15:40:59 -0300

    Estruturando HOF

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-1-javascript-a-higher-order-function/Para Fixar/estruturando-hof.js
@@ -0,0 +1,7 @@
+const acordando = () => 'Acordando!';
+const cafe = () => 'Bora tomar café!!';
+const dormir = () => 'Partiu dormir!!';
+
+const doingThings = (acao) => acao();
+
+console.log(doingThings(acordando));
